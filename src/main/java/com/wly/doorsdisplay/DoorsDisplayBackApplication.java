c43ad3e89aade58953c8748c7fe973c787b4716a
Moonwly <1241076850@qq.com>
2019-09-10 11:26:56 +0800

    Initial commit

---
+++ src/main/java/com/wly/doorsdisplay/DoorsDisplayBackApplication.java
@@ -0,0 +1,13 @@
+package com.wly.doorsdisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorsDisplayBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoorsDisplayBackApplication.class, args);
+    }
+
+}
